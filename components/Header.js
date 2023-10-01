64a9c8744fc07a679b0c68a80fb4e359d29931bd
Lucky <luckysolanki902@gmail.com>
2023-10-01 17:17:23 +0530

    removed animation

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // import './styles/Header.css'
-import ParticleAnimation from './particleanimation';
+// import ParticleAnimation from './particleanimation';
 
 const Header = () => {
   return (
     <header>
-      <ParticleAnimation/>
+      {/* <ParticleAnimation/> */}
       <h1>EasyMola ( HBTU Kanpur ) </h1>
       <p className='tagline'>Earn While riding</p>
     </header>
