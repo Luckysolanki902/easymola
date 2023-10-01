5633b2c0ebad377bff60e86144cde969147d3c39
Lucky <luckysolanki902@gmail.com>
2023-10-01 17:41:58 +0530

    fucking animation

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
-// import './styles/Header.css'
-// import ParticleAnimation from './particleanimation';
+
 
 const Header = () => {
   return (
     <header>
-      {/* <ParticleAnimation/> */}
       <h1>EasyMola ( HBTU Kanpur ) </h1>
       <p className='tagline'>Earn While riding</p>
     </header>
